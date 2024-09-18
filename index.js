daaa28d093b6ad5ae04fbf20c3a806633b2cde3c
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-09-18 08:27:23 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+// has a function expression called divide ✓
+const divide = (a,b) =>  {
+    console.log(`divides ${a}`); //remember you cant use numbers in the parameters so we assign variables as echoed in square and add
+    console.log(`divides ${b}`);
+    return a / b;
+  }
+  divide(2000,100); 
+
+
+   //has an arrow function called square ✓
+   //square arrow function takes one parameter and multiplies it times itself✓
+  const square = (x) => x * x;        //read errors correctly test 3-4 is seperate from test 1-2 its slows you down 
+
+const add = (c,d) => c + d;
+
